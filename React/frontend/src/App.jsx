a9ea42aa0ee8dd9158e68ecef1bd76547d9008dc
dkt882 <kumardharmedra578@gmial.com>
2026-07-09 00:11:39 +0530

    frontend design

---
+++ React/frontend/src/App.jsx
@@ -0,0 +1,92 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+
+
+
+
+
+      <div class="style-1">
+        <nav class="style-2">
+          <div class="style-3">
+            <div class="style-4"><a href="/" class="style-5">Frontend Practice</a></div>
+            <div class="style-6"><button class="style-7"><img src="/icons/hamburger-menu-50.png" alt="" class="style-8" /></button>
+              <ul class="style-9">
+                <div class="style-10"><a href="/" class="style-11">Home</a><a href="/projects" class="style-12">Projects</a><a href="/faq" class="style-13">FAQ</a></div>
+              </ul>
+            </div>
+            <div class="style-14"><a href="/" class="style-15">Home</a><a href="/projects" class="style-16">Projects</a><a href="/faq" class="style-17">FAQ</a></div>
+          </div>
+        </nav>
+        <main class="style-18">
+          <section class="style-19">
+            <div class="style-20"></div>
+            <div class="style-21">
+              <div class="style-22">
+                <div class="style-23">
+                  <div class="style-24">
+                    <h2 class="style-25">Become a better frontend developer.</h2>
+                    <p class="style-26">Take your frontend skills to the next level by recreating <strong class="style-27">real websites</strong> from <strong class="style-28">real companies</strong>.</p>
+                    <p class="style-29">The perfect type of practice for developers of all skill levels. Are you up for the challenge?</p>
+                    <div class="style-30"><a href="/projects" class="style-31">View Projects</a><a href="#hdiw" class="style-32">Learn More</a></div>
+                  </div>
+                  <div class="style-33"><img src="https://www.frontendpractice.com/project-collage.png" alt="thumbnail" class="style-34" /></div>
+                </div>
+              </div>
+              <div class="style-35">
+                <h2 class="style-36">Frontend Practice provides you with the inspiration and resources to create beautiful websites.</h2>
+                <p class="style-37">Here are some ways you can benefit from using our platform:</p>
+                <div class="style-38">
+                  <div class="style-39">
+                    <div class="style-40"><img alt="globe with a search bar" src="https://images.prismic.io/frontendpractice/f990db40-ba07-479c-b52c-3b2c6caabc89_domain.png?auto=compress,format" class="style-41" />
+                      <p class="style-42">Enhance your skills by building real websites selected for their educational value.</p>
+                    </div>
+                    <div class="style-43"><img alt="4 part grid of shapes" src="https://images.prismic.io/frontendpractice/65eee214-bbfe-4f9e-ad0f-70f7785679be_layout.png?auto=compress,format" class="style-44" />
+                      <p class="style-45">Test and improve your frontend knowledge through various challenges.</p>
+                    </div>
+                    <div class="style-46"><img alt="a color palette sheet" src="https://images.prismic.io/frontendpractice/8d3c1f70-6ff3-41f0-b9d6-3b46f2899359_pantone.png?auto=compress,format" class="style-47" />
+                      <p class="style-48">Use the provided color palettes for each project to save time.</p>
+                    </div>
+                    <div class="style-49"><img alt="lightbulb turning on" src="https://images.prismic.io/frontendpractice/d6e88860-7347-4d32-acf8-aa1f01db8685_idea.png?auto=compress,format" class="style-50" />
+                      <p class="style-51">Find helpful, curated resources to tackle challenging elements.</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="style-52">
+                  <h2 class="style-53">Ready to become a better frontend developer?</h2><a href="/projects" class="style-54">Get Started</a>
+                </div>
+              </div>
+            </div>
+          </section>
+        </main>
+        <footer class="style-55">
+          <section class="style-56">
+            <div class="style-57">
+              <div class="style-58">
+                <p class="style-59">Frontend Practice - © 2026</p>
+              </div>
+              <div class="style-60"><a href="/" class="style-61">Home</a><a href="/projects" class="style-62">Projects</a><a href="/faq" class="style-63">FAQ</a></div>
+            </div>
+          </section>
+        </footer>
+      </div>
+      <next-route-announcer class="style-64">
+        <p aria-live="assertive" role="alert" class="style-65"></p>
+      </next-route-announcer>
+      <veepn-lock-screen class="style-66"></veepn-lock-screen>
+
+
+
+
+
+
+
+    </>
+  )
+}
+
+export default App
