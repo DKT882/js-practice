4ddac9247fef93627f3155fadb17174a5ae90bc0
dkt882 <kumardharmedra578@gmail.com>
2026-07-11 19:01:55 +0530

    useContext

--- React/hooks/src/components/UseContext.jsx
+++ React/hooks/src/components/UseContext.jsx
@@ -5,18 +5,30 @@ import Child1 from './useContext/Child1';
 // step 2: wrap all the child in provider
 // step 3: pass value
 // step 4: consum inside the consumer file
-const Context = createContext()
+// const Context = createContext()
+const ThemeContext=createContext()
 
 function UseContext() {
-    const [userData, setUserData] = useState({name:'dkt'});
+    // const [userData, setUserData] = useState({name:'dkt'});
+    const [theme, setTheme] = useState('light');
     return (
         <>
-            <Context.Provider value={userData}>
-                <Child1></Child1>
-            </Context.Provider>
+            <ThemeContext.Provider value={{theme,setTheme}}>
+                <div id='thme' style={{backgroundColor:theme==='light'?'beige':'black'}}>
+                    <Child1></Child1>
+                </div>
+            </ThemeContext.Provider>
         </>
     )
+    // return (
+    //     <>
+    //         <Context.Provider value={userData}>
+    //             <Child1></Child1>
+    //         </Context.Provider>
+    //     </>
+    // )
 }
 
 export default UseContext
-export {Context}
+export {ThemeContext}
+// export {Context}
--- React/hooks/src/components/useContext/Child3.jsx
+++ React/hooks/src/components/useContext/Child3.jsx
@@ -1,15 +1,30 @@
 import { useContext } from 'react'
-import {Context} from '../UseContext.jsx'
+// import {Context} from '../UseContext.jsx'
+import {ThemeContext} from '../UseContext.jsx'
 
 function Child3() {
-    const user = useContext(Context)
+    const {theme,setTheme} = useContext(ThemeContext)
+    const handleClick =() => {
+        if(theme==='light')
+            setTheme('dark')
+        else
+            setTheme("light")
+    }
     return (
         <>
         <h1 style={{color:'black'}}>
-            data : {user.name}
+            <button onClick={handleClick}>Click me</button>
         </h1>
         </>
     )
+    // const user = useContext(Context)
+    // return (
+    //     <>
+    //     <h1 style={{color:'black'}}>
+    //         data : {user.name}
+    //     </h1>
+    //     </>
+    // )
 }
 
 export default Child3
