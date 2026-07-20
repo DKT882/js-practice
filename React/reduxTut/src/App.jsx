5f063a10b6568c2a5127e6f90e1e3a73730b1a5e
dkt882 <kumardharmedra578@gmail.com>
2026-07-20 16:42:38 +0530

    inc-dec-reset btn using redux-toolkit

--- React/reduxTut/src/App.jsx
+++ React/reduxTut/src/App.jsx
@@ -1,15 +1,29 @@
-import { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+
 import './App.css'
+import { decrement, increment, reset } from './features/counters/CountSlice'
 
 function App() {
-  const [count, setCount] = useState(0)
-  function HandleClick(){
-    setCount(count+1)
+  const count = useSelector((state) => state.counter.value)
+  const dispatch = useDispatch()
+  function handleIncrementclick() {
+    dispatch(increment())
+
+  }
+  function handleDedrementClick() {
+    dispatch(decrement())
+  }
+  function handleResetClick() {
+    dispatch(reset())
   }
   return (
-    <>
-      <div onClick={HandleClick}>{count}</div>
-    </>
+    <div className='container' >
+      <button onClick={handleIncrementclick}>+</button>
+      <p>count: {count}</p>
+      <button onClick={handleDedrementClick}>-</button>
+      <button onClick={handleResetClick}>Reset</button>
+
+    </div>
   )
 }
 
