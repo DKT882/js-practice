7e5000049a5efa9870c8adcd32709c754b666330
dkt882 <kumardharmedra578@gmail.com>
2026-07-21 14:50:04 +0530

    navBar

--- React/Assignment1/src/App.jsx
+++ React/Assignment1/src/App.jsx
@@ -1,15 +1,25 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import NavBar from './features/shared/NavBar'
+import Homepage from './features/home/Homepage'
+import Profile from './features/profile/pages/Profile'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <BrowserRouter>
+      <NavBar></NavBar>
+        <Routes>
+          <Route path='/' element={<Homepage></Homepage>}></Route>
+          <Route path='/home' element={<Homepage></Homepage>}></Route>
+          <Route path='/course' element={<Homepage></Homepage>}></Route>
+          <Route path='/profile' element={<Profile></Profile>}></Route>
+          <Route path='/*' element={<div>Error 404: PageNot Found Try /about or /home or /</div>}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
--- React/Assignment1/src/features/shared/NavBar.jsx
+++ React/Assignment1/src/features/shared/NavBar.jsx
@@ -1,9 +1,20 @@
+import { NavLink } from 'react-router-dom'
 import React from 'react'
 
 const NavBar = () => {
   return (
-    <div>
-      
+    <div className='flex bg-gray-800 justify-around items-center'>
+      <div className="navImage py-4">
+        <NavLink to='/'>
+        <img src="https://avatars.githubusercontent.com/u/107047333?v=4" alt="ProfileImage" className='h-15 rounded-full'/>
+        </NavLink>
+      </div>
+      <ul className='flex gap-25  text-2xl py-4'>
+        <li className='hover:text-green-600 transition-all duration-300 ease-in-out'><NavLink to='/'>Home</NavLink></li>
+        <li className='hover:text-green-600 transition-all duration-300 ease-in-out'><NavLink to='/course'>Courses</NavLink></li>
+        <li className='hover:text-blue-600 transition-all duration-300 ease-in-out'><NavLink to='/profile'>Profile</NavLink></li>
+        
+      </ul>
     </div>
   )
 }
