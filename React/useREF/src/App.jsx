c68cc73c5ade6906c961cf985c009d350afa7b20
dkt882 <kumardharmedra578@gmail.com>
2026-07-24 12:43:55 +0530

    2 examples : 1- use as veriable in which it Persisting Values Across Re-renders and 2- directly access and menipulate DOM element

--- React/useREF/src/App.jsx
+++ React/useREF/src/App.jsx
@@ -1,11 +1,39 @@
 import { useState } from 'react'
 import './App.css'
+import { useEffect } from 'react'
+import { useRef } from 'react'
 
 function App() {
+  // eg:1 
+/*  const [count,setCount]=useState(0)
+  const val=useRef(0)
 
+  
+  function handleClick(){
+    val.current=val.current+1
+    console.log('val.current is ',val.current)
+    setCount(count+1)
+  }
+  useEffect(() => {
+  console.log('count updated')
+  })
   return (
     <>
+      <p>{count}</p>
+      <button onClick={handleClick}>Click me </button>
     </>
+  )*/
+  
+  let btnRef=useRef()
+  function changeColor(){
+    // console.log(btnRef.current)
+    btnRef.current.style.backgroundColor='red'
+  }
+  return (
+    <div>
+      <button ref={btnRef} >Thin button color will change with clicking other button</button>
+      <button onClick={changeColor}>Click me to change the color</button>
+    </div>
   )
 }
 
