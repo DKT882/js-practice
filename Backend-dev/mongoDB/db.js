bf8b651fb8b893041a18bcaaf5adedfb5fbf91bf
dkt882 <kumardharmedra578@gmail.com>
2026-07-28 17:21:59 +0530

    schema and model created

--- Backend-dev/mongoDB/db.js
+++ Backend-dev/mongoDB/db.js
@@ -1,12 +1,11 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config()
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect('mongodb+srv://kumardharmedra578_db_user:PAmdPSGjUptO3nLv@cluster0.98hrxxn.mongodb.net/test', {
-            // Forces Node to use IPv4 instead of IPv6
-            family: 4 
-        });
-        
+        const conn = await mongoose.connect(process.env.MONGODB_URI);
         console.log(`MongoDB connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error: ${error.message}`);
