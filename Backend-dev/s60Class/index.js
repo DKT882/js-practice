88048cb303a322b98736a090bd0893205c3b93fd
dkt882 <kumardharmedra578@gmail.com>
2026-07-30 15:19:54 +0530

    server started

--- Backend-dev/s60Class/index.js
+++ Backend-dev/s60Class/index.js
@@ -1,2 +1,14 @@
 // creatre server only
 
+import express from 'express';
+const app=express()
+const PORT=3000;
+
+
+app.get('/',(req,res) => {
+    res.send('Hello boy')
+})
+
+app.listen(PORT,() => {
+    console.log(`Hello boy this is server http://localhost:${3000}/ `)
+})
