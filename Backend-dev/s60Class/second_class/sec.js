6e95cd4c45262db565b114ddd94938409df6cd0f
dkt882 <kumardharmedra578@gmail.com>
2026-08-04 15:32:00 +0530

    put req

--- Backend-dev/s60Class/second_class/sec.js
+++ Backend-dev/s60Class/second_class/sec.js
@@ -38,8 +38,8 @@ app.put("/api/:id", (req, res) => {
     const id = Number(req.params.id);
     const student = students.find((s) => s.id === id);
     if (!student) return res.status(404).json({ error: "Student not found" });
-    student.name = req.body.name;
-    student.course = req.body.course;
+    students.name = req.body.name;
+    students.course = req.body.course;
     res.status(200).json(student);
 });
 
