442a335e5f3064c7c006207a6a17cb074bf78a98
dkt882 <kumardharmedra578@gmail.com>
2026-08-10 14:58:27 +0530

    localhost

--- Backend-dev/s60Class/apiTesting.js
+++ Backend-dev/s60Class/apiTesting.js
@@ -36,5 +36,5 @@ app.delete("/users/:ids", (req, res) => {
 
 
 app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+    console.log("Server is running on port http://localhost:3000");
 });
