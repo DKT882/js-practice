7e2f24da80473b162467d523cc2be27162dd3c34
dkt882 <kumardharmedra578@gmail.com>
2026-08-12 14:13:08 +0530

    cors s-60

--- fullstackProject/backend/src/index.js
+++ fullstackProject/backend/src/index.js
@@ -1,11 +1,20 @@
 // require('dotenv').config({path: './env'});
 import dotenv from 'dotenv';
 import mongoose from "mongoose";
+import express from "express";
 import connectDB from "./db/index.js";
 import { DB_NAME } from "./content.js";
-
+const app=express()
 dotenv.config()
 await connectDB()
+.then(() => {
+    app.listen(process.env.PORT||8000,() => {
+        console.log(`Server is running on port ${process.env.PORT}`)
+    })
+})
+.catch((err) => {
+    console.log(`MongoDB connection error : ${err}`)
+})
 
 
 
