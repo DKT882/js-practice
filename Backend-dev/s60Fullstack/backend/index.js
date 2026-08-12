564b996b0f1ec082f7035d1e2f0b1bc2184c2f27
dkt882 <kumardharmedra578@gmail.com>
2026-08-12 14:30:58 +0530

    frontend connetion with backend

---
+++ Backend-dev/s60Fullstack/backend/index.js
@@ -0,0 +1,47 @@
+// creatre server only
+
+import express from 'express';
+const app=express()
+const PORT=3000;
+
+app.use(express.json());
+
+app.get('/',(req,res) => {
+    res.send('Hello boy')
+})
+
+const users = [
+  { id: 1, name: "John Doe" },
+  { id: 2, name: "Jane Smith" },
+  { id: 3, name: "Alice Johnson" },
+];
+
+app.get("/users", (req, res)=>{
+    res.status(200).json(users);
+})
+
+app.post("/users", (req,res) => {
+    const newUser = {
+        id:users.length + 1,
+        name:req.body.name
+    }
+    users.push(newUser);
+    res.status(201).json(newUser);
+})
+
+app.put("/users/:ids", (req,res) => {
+    const id = Number(req.params.ids);
+    const user = users.find((u) => u.id === id);
+    if(!user) return res.status(404).json({message: "User not found"});
+    user.name = req.body.name;
+    res.status(200).json(user);
+})
+app.delete("/users/:ids", (req,res) => {
+    const id = Number(req.params.ids);
+    users = users.filter((u) => u.id !== id);
+    res.status(204).send();
+})
+
+app.listen(PORT,() => {
+    console.log(`Hello boy this is server http://localhost:${3000}/ `)
+})
