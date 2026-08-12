6bc7577d491207f05ab6c7da25b149b1fde1cfbc
dkt882 <kumardharmedra578@gmail.com>
2026-08-12 15:29:44 +0530

    project done

--- Backend-dev/s60Fullstack/backend/index.js
+++ Backend-dev/s60Fullstack/backend/index.js
@@ -1,47 +1,57 @@
 // creatre server only
 
 import express from 'express';
-const app=express()
-const PORT=3000;
+import cors from 'cors'
+const app = express()
+const PORT = 3000;
+app.use(cors())
 
 app.use(express.json());
 
-app.get('/',(req,res) => {
+app.get('/', (req, res) => {
     res.send('Hello boy')
 })
 
-const users = [
-  { id: 1, name: "John Doe" },
-  { id: 2, name: "Jane Smith" },
-  { id: 3, name: "Alice Johnson" },
+let users = [
+    { id: 1, name: "John Doe" },
+    { id: 2, name: "Jane Smith" },
+    { id: 3, name: "Alice Johnson" },
+];
+let students = [
+    { id: "st1", name: "DKT" },
+    { id: "st2", name: "KY" },
+    { id: "st3", name: "JSB" },
 ];
 
-app.get("/users", (req, res)=>{
+app.get("/users", (req, res) => {
     res.status(200).json(users);
 })
+app.get("/students", (req, res) => {
+    res.status(200).json(students);
+})
 
-app.post("/users", (req,res) => {
+app.post("/users", (req, res) => {
     const newUser = {
-        id:users.length + 1,
-        name:req.body.name
+        id: users.length + 1,
+        name: req.body.name
     }
     users.push(newUser);
     res.status(201).json(newUser);
 })
 
-app.put("/users/:ids", (req,res) => {
+app.put("/users/:ids", (req, res) => {
     const id = Number(req.params.ids);
     const user = users.find((u) => u.id === id);
-    if(!user) return res.status(404).json({message: "User not found"});
+    if (!user) return res.status(404).json({ message: "User not found" });
     user.name = req.body.name;
     res.status(200).json(user);
 })
-app.delete("/users/:ids", (req,res) => {
+app.delete("/users/:ids", (req, res) => {
     const id = Number(req.params.ids);
     users = users.filter((u) => u.id !== id);
     res.status(204).send();
 })
 
-app.listen(PORT,() => {
+app.listen(PORT, () => {
     console.log(`Hello boy this is server http://localhost:${3000}/ `)
 })
--- Backend-dev/s60Fullstack/frontend/src/App.jsx
+++ Backend-dev/s60Fullstack/frontend/src/App.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
-
+import { useEffect } from 'react'
+import { useState } from 'react'
 const App = () => {
+  const [userdata, setuserData] = useState([])
+  const [studentdata, setstudentData] = useState([])
+  const fetchData = async () => {
+    const response = await fetch('http://localhost:3000/users')
+    setuserData(await response.json())
+
+    // student data response 
+    const responseStudents = await fetch('http://localhost:3000/students')
+    setstudentData(await responseStudents.json())
+    console.log(responseStudents)
+  };
+  useEffect(() => {
+    fetchData()
+  }, [])
   return (
-    <div>
-      
-    </div>
+    <>
+      <div>
+        {userdata.map((item) => (
+          <div key={item.id}>{item.name}</div>
+        ))}
+        <br />
+        <h1>Student data: </h1>
+        {studentdata.map((item) => (
+          <div key={item.id}>{item.name}</div>
+        ))}
+      </div>
+    </>
   )
 }
 
