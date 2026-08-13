2bbb45afb96e591b59bd2931bfe9323718b770b1
dkt882 <kumardharmedra578@gmail.com>
2026-08-13 15:00:14 +0530

    Custom api response and error handling

---
+++ fullstackProject/backend/src/utils/ApiResponse.js
@@ -0,0 +1,8 @@
+class ApiResponse{
+    constructor(statusCode,data,messag="Success"){
+        this.statusCode=statusCode
+        this.data = data
+        this.message=this.message
+        this.success=statusCode
+    }
+}
