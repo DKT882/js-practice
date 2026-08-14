f506cecb4af074219b2186f6bc5e893d409e5280
dkt882 <kumardharmedra578@gmail.com>
2026-08-14 17:31:13 +0530

    User and video model with hooks and JWT v2

--- fullstackProject/backend/src/models/user.model.js
+++ fullstackProject/backend/src/models/user.model.js
@@ -1,4 +1,7 @@
 import mongoose, { Schema } from "mongoose"
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt" 
+// we can't directly encrpt so we use hooks of mongoose / middleware "prehook"
 
 const userSchema = new Schema(
     {
@@ -45,5 +48,42 @@ const userSchema = new Schema(
 
     }, { timestamps: true }
 )
+userSchema.pre("save",async function (next){
+    if (!this.isModified("password")) return next() // in this it check if password not change then direcdtly return next().
+
+    this.password=bcrypt.hash(this.password,10)
+    next()
+})
+
+userSchema.methods.isPasswordCorrect=async function(password){
+    return await bcrypt.compare(password,this.password) // it compairs encrypted pass and non-encrypted pass for confirmation and then returen true or false. 
+}
+
+userSchema.methods.generateAccessToken = function(){
+    return jwt.sign({
+        _id:this._id,
+        email:this.email,
+        username:this.username,
+        fullName:this.fullName
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+    }
+)
+} //-> to generate access token
+
+
+userSchema.methods.generateRefreshToken =function(){
+    return jwt.sign({
+        _id:this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+        expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+    }
+) 
+}// -> to generate refresh token. it has less details.
+
 
 export const User = mongoose.model("User", userSchema)
