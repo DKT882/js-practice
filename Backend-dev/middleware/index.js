e0b10e9bbaf92282591f53e1820172732716a11b
dkt882 <kumardharmedra578@gmail.com>
2026-08-18 15:34:15 +0530

    dfavs

--- Backend-dev/middleware/index.js
+++ Backend-dev/middleware/index.js
@@ -55,7 +55,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use((req, res, next) => {
-    console.log(${req.method} ${req.url})
+    console.log(`${req.method} ${req.url}`)
     next()
 });
 
